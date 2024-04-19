32fc67a2fdfef9e27055eaa0338b06d026830b65
unknown <bekibstech@gmail.com>
2024-04-19 13:58:32 -0300

    rotas funcionando

--- easyfood/app/Http/Controllers/PedidosController.php
+++ easyfood/app/Http/Controllers/PedidosController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 abstract class PedidosController
 {
-    public function quadro (){
-        return view('pedidos.quadro');
+    public function quadro(){
+        return view('/pedidos/quadro');
     }
 }
