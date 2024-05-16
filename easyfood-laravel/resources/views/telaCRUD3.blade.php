1a25978d2c55e8e1bcf1041d3ed2d1a94f7c0034
unknown <aigor2407@gmail.com>
2024-05-16 14:00:21 -0300

    novas telas

--- easyfood-laravel/resources/views/telaCRUD3.blade.php
+++ easyfood-laravel/resources/views/telaCRUD3.blade.php
@@ -1,126 +1,100 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="widtd=device-widtd, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Gerenciamento de itens</title>
-    @vite(['resources/css/style.css']);
+    @vite(['resources/css/teste2.css']);
 </head>
+
 <body>
 
-    <img class="logo2" src="/assets/img/easy-food.PNG">
+    <img class="logo" src="IMG/logo.jpg">
     <p>
-    <button style="background-color:#0D127B; border-color:#2517bf; color:white">
-        CARDÁPIO</button>
-    <button style="background-color:#0D127B; border-color:#2517bf; color:white">
-         ITENS</button>
-    <button style="background-color:#0D127B; border-color:#2517bf; color:white">
-            PEDIDOS</button>
-     <button style="background-color:#0D127B; border-color:#2517bf; color:white">
-                COZINHA</button>
-     <button style="background-color:#0D127B; border-color:#2517bf; color:white">
-                    FECHAMENTO</button>
-
-
-    <div id="itens">
+    <section class="categorias">
+        <button class="categoria">CARDÁPIO</button>
+        <button class="categoria">ITENS</button>
+        <button class="categoria">PEDIDOS</button>
+        <button class="categoria">COZINHA</button>
+        <button class="categoria">FECHAMENTO</button>
+    </section>
+    <section class="orders">
+        <br>
         <h2>DOCES:</h2>
-
-      <a href="/">  <button class = "botao2" style="background-color:#0D127B; border-color:#2517bf; color:white">
-            VOLTAR</button></a>
-
-            <br><br>
-        
-       
-
-        
-
-        <table id="tabela-itens">
-            <thead>
+        <br>
+        <table>
+            <tdead>
                 <tr>
-                    <th>Item</th>
-                    <th>Ingrediente</th>
-                    <th>Valor</th>
-                    
+                    <td>Item</td>
+                    <td>Ingredientes</td>
+                    <td>Valor</td>
+                    <td>Ação</td>
                 </tr>
-            </thead>
+            </tdead>
             <tbody>
-                </tbody>
+                <tr>
+                    <td>TORTA HOLANDESA</td>
+                    <td>Biscoito com creme de Chocolate e creme Branco </td>
+                    <td>R$: 80,00</td>
+                    <td>
+                        <button type="button" class="button ed">Editar</button>
+                        <button type="button" class="button ex">Excluir</button>
+                    </td>
+                </tr>
+                <tr>
+                    <td>TORTA DE LIMÃO</td>
+                    <td>Creme de limão com massa de biscoito e pedaços de Limão</td>
+                    <td>R$: 80,00</td>
+                    <td>
+                        <button type="button" class="button ed">Editar</button>
+                        <button type="button" class="button ex">Excluir</button>
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>PETIT GÂTEAU</td>
+                    <td>Pequeno bolo recheado com sorvete de Baunilhia</td>
+                    <td>R$: 25,00</td>
+                    <td>
+                        <button type="button" class="button ed">Editar</button>
+                        <button type="button" class="button ex">Excluir</button>
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>PUDIM</td>
+                    <td>Pudim de leite</td>
+                    <td>R$: 45,00</td>
+                    <td>
+                        <button type="button" class="button ed">Editar</button>
+                        <button type="button" class="button ex">Excluir</button>
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>BOLO GELADO</td>
+                    <td>Bolo branco com coco gelado</td>
+                    <td>R$: 16,00</td>
+                    <td>
+                        <button type="button" class="button ed">Editar</button>
+                        <button type="button" class="button ex">Excluir</button>
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>ARROZ DOCE</td>
+                    <td>Arroz cozido com leite condesado e canela</td>
+                    <td>R$: 20,00</td>
+                    <td>
+                        <button type="button" class="button ed">Editar</button>
+                        <button type="button" class="button ex">Excluir</button>
+                    </td>
+                </tr>
+            </tbody>
         </table>
-
-        <table id="tabela-itens-dois">
-            <thead>
-                <tr>
-                    <th>TORTA HOLANDESA</th>
-                    <th>Biscoito com creme de Chocolate e creme Branco </th>
-                    <th>R$: 80,00</th>
-                   
-                </tr>
-            </thead>
-
-            <thead>
-                <tr>
-                    <th>TORTA DE LIMÃO</th>
-                    <th>Creme de limão com massa de biscoito e pedaços de Limão</th>
-                    <th>R$: 80,00</th>
-                   
-                </tr>
-            </thead>
-
-            <thead>
-                <tr>
-                    <th>PETIT GÂTEAU</th>
-                    <th>Pequeno bolo recheado com sorvete de Baunilhia</th>
-                    <th>R$: 25,00</th>
-                   
-                </tr>
-            </thead>
-
-            <thead>
-                <tr>
-                    <th>PUDIM</th>
-                    <th>Pudim de leite</th>
-                    <th>R$: 45,00</th>
-                   
-                </tr>
-            </thead>
-
-            <thead>
-                <tr>
-                    <th>BOLO GELADO</th>
-                    <th>Bolo branco com coco gelado</th>
-                    <th>R$: 16,00</th>
-                   
-                </tr>
-            </thead>
-
-            <thead>
-                <tr>
-                    <th>ARROZ DOCE</th>
-                    <th>Arroz cozido com leite condesado e canela</th>
-                    <th>R$: 20,00</th>
-                   
-                </tr>
-            </thead>
-
-            <tbody>
-                </tbody>
-        </table>
-
-        
-
-
-
-    </div>
-
-    <button class = "botao2" style="background-color:#0D127B; border-color:#2517bf; color:white">
-            ADICIONAR</button>
-
-    <button class = "botao2" style="background-color:#0D127B; border-color:#2517bf; color:white">
-            EXCLUIR</button>
-
-    <button class = "botao2" style="background-color:#0D127B; border-color:#2517bf; color:white">
-            EDITAR</button>
-
+    </section>
 </body>
+
 </html>
